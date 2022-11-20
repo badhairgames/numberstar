1390881d13353a6b2f435df0118659000363921a
badhairgames <go@badhairgames.com>
2022-11-20 16:10:42 +0000

    Adjust text width

--- scripts/utils/text.js
+++ scripts/utils/text.js
@@ -8,6 +8,8 @@ class Text {
     drawCenteredText(msg, x, y, size, colour, style) {
         this.ctx.font = `${style} ${size}px sans-serif`;
         this.ctx.fillStyle = colour;
+        size = this.adjustToMaxWidth(size, style, msg);
+
         let text = this.ctx.measureText(msg);
         let x1 = x - (text.width / 2);
         let y1 = y + (size / 3);
@@ -21,8 +23,10 @@ class Text {
     }
 
     drawBigMessage(msg, colour) {
-        this.ctx.font = '60pt sans-serif';
+        this.ctx.font = '60px sans-serif';
         this.ctx.fillStyle = colour;
+        this.adjustToMaxWidth(60, '', msg);
+
         let text = this.ctx.measureText(msg);
         let msgX = (this.width - text.width) / 2;
         let msgY = (this.height - 60) / 2;
@@ -30,12 +34,24 @@ class Text {
     }
 
     drawSmallMessage(msg, colour) {
-        this.ctx.font = '30pt sans-serif';
+        this.ctx.font = '30px sans-serif';
         this.ctx.fillStyle = colour;
+        this.adjustToMaxWidth(60, '', msg);
+
         let text = this.ctx.measureText(msg);
         let msgX = (this.width - text.width) / 2;
         let msgY = (this.height + 30) / 2;
         this.ctx.fillText(msg, msgX, msgY);
+    }
+
+    adjustToMaxWidth(size, style, msg) {
+        const width = this.ctx.measureText(msg).width;
+        if (width > this.width) {
+            size = size * this.width / width;
+            this.ctx.font = `${style} ${size}px sans-serif`;
+        }
+
+        return size;
     }
 }
 
