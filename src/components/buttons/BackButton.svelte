999fcafe5b70687a39468f15f8667c368201e3be
badhairgames <go@badhairgames.com>
2023-02-25 20:21:57 +0000

    Replace icons with SVG

--- src/components/buttons/BackButton.svelte
+++ src/components/buttons/BackButton.svelte
@@ -4,23 +4,24 @@
     const dispatch = createEventDispatcher();
 </script>
 
-<button on:click={() => dispatch('back')}>🡐</button>
+<button on:click={() => dispatch('back')}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="svg-icon"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg></button>
 
 <style lang="scss">
     button {
         position: absolute;
         background: transparent;
-        color: #777;
-        width: auto;
         left: 0;
         top: 0;
-        font-size: 2em;
-        padding: 0;
-        margin: 0;
 
-        &:hover {
-            background: transparent;
-            color: #333;
+        .svg-icon {
+            fill: #777;
+            height: 2em;
+            width: 2em;
+
+            &:hover {
+                fill: #0D6FA7;
+                cursor: pointer;
+            }
         }
     }
 </style>
--- src/components/buttons/CloseButton.svelte
+++ src/components/buttons/CloseButton.svelte
@@ -4,24 +4,27 @@
     const dispatch = createEventDispatcher();
 </script>
 
-<button on:click={() => dispatch('close')}>×</button>
+<button on:click={() => dispatch('close')}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="svg-icon"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+</button>
 
 
 <style lang="scss">
     button {
         position: absolute;
         background: transparent;
-        color: #777;
-        width: auto;
         right: 0;
         top: 0;
-        font-size: 2em;
-        padding: 0;
-        margin: 0;
 
-        &:hover {
-            background: transparent;
-            color: #333;
+        .svg-icon {
+            fill: #777;
+            height: 2em;
+            width: 2em;
+
+            &:hover {
+                fill: #0D6FA7;
+                cursor: pointer;
+            }
         }
     }
 </style>
