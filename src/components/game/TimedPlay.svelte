ef38bd0c32ff24959257263b1077ddbcc641f7d1
badhairgames <go@badhairgames.com>
2023-02-25 20:26:13 +0000

    Refactor timed game

--- src/components/game/TimedPlay.svelte
+++ src/components/game/TimedPlay.svelte
@@ -5,15 +5,19 @@
     import Timer from './Timer.svelte';
     import Info from './Info.svelte';
     import Options from './Options.svelte';
+    import QuestionDisplay from './QuestionDisplay.svelte';
 
     export let game: Game;
     const dispatch = createEventDispatcher();
 
     let currentQuestion: Question = null;
+    let questionComponent;
     let optionsComponent;
     let timerComponent;
     let timeForGame = game.defaultGameTime;
     let showLives = false;
+    let questionWidth;
+    let questionHeight;
 
     function resetQuestion() {
         currentQuestion = new Question(game, currentQuestion);
@@ -41,20 +45,68 @@
     resetQuestion();
 </script>
 
-{#if currentQuestion}
+<!--{#if currentQuestion}
     <div class="container">
         <Info bind:game bind:showLives />
         <div class="question">{currentQuestion.content}</div>
         <Options bind:currentQuestion on:answer={answered} bind:this={optionsComponent} />
         <Timer on:timeout={outOfTime} bind:time={timeForGame} bind:this={timerComponent} />
     </div>
+{/if}-->
+
+{#if currentQuestion}
+    <div class="container">
+        <div class="info">
+            <Info bind:game bind:showLives />
+        </div>
+        <div
+            class="question"
+            bind:this={questionComponent}
+            bind:clientWidth={questionWidth}
+            bind:clientHeight={questionHeight}
+        >
+            <QuestionDisplay
+                bind:question={currentQuestion.content}
+                bind:containerWidth={questionWidth}
+                bind:containerHeight={questionHeight}
+            />
+        </div>
+        <div class="options">
+            <Options bind:currentQuestion on:answer={answered} bind:this={optionsComponent} />
+        </div>
+        <div class="timer">
+            <Timer on:timeout={outOfTime} bind:time={timeForGame} bind:this={timerComponent} />
+        </div>
+    </div>
 {/if}
 
 <style lang="scss">
     .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100vh;
+
+        .info {
+            flex: 1;
+        }
+
         .question {
-            font-size: 3em;
+            position: relative;
             font-weight: bold;
+            flex: 3;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .options {
+            flex: 3;
+        }
+
+        .timer {
+            flex: 2;
+            padding-bottom: 1em;
         }
     }
 </style>
